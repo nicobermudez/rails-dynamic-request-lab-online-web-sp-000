3b550f810c1199daf438486d1e53084355e810e4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-12 15:45:21 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,14 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    set_student
+  end
+
+private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
